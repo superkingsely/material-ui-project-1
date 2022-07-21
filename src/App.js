05b68ac7d08c866d5e16873866d1b202d1f451ae
chijioke <superkingsely@gmail.com>
2022-07-21 11:06:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Typography, CssBaseline, AppBar,Toolbar, Container,Grid,Button } from '@mui/material';
+import AddAPhotoRoundedIcon from '@mui/icons-material/AddAPhotoRounded';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    return ( 
+        <div>
+            <CssBaseline/>
+
+            <AppBar position='relative'>
+                
+                <Toolbar >
+                    <AddAPhotoRoundedIcon/>
+                    <Typography variant="h6" >
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth='sm' >
+                        <Typography
+                        variant='h2'
+                        align='center'
+                        color='textPrimary'
+                        gutterBottom
+                        >
+                        Photo Album
+                        </Typography>
+                        <Typography
+                        variant='h5'
+                        align='center'
+                        color='textSecondary'
+                        paragraph
+                        >
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nulla obcaecati voluptatem expedita sapiente esse fugiat dolor, molestias sunt deleniti.
+                        </Typography>
+
+                        <div>
+                            <Grid container spacing={2} justifyContent='center'>
+                                <Grid item>
+                                    <Button variant='contained'
+                                    color='primary'
+                                    >
+                                        See my photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined'
+                                    color='primary'
+                                    >
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
+        </div>
+     );
 }
-
+ 
 export default App;
